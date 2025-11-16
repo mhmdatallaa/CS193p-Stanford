e1b54a69145c579d3cc57dcbccecfa71be10a4ae
Mohamed Attallah <mohamed.atallah440@gmail.com>
2025-11-16 06:26:24 +0200

    New Project

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Mohamed Atallah on 16/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/CodeBreaker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by Mohamed Atallah on 16/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
